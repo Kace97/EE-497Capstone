7d9d18739170ca7b4d6770eaed8690191a39c33f
Charlie Fisher <fosh@uw.edu>
2020-04-08 18:58:14 -0700

    update code

--- integration/runwholecode.py
+++ integration/runwholecode.py
@@ -10,5 +10,7 @@ str_time = now.strftime("%H:%M:%S")
 print("time =", str_time)
 
 for i in range(5):
-    GPIO.output(pin,GPIO.HIGH)
+    GPIO.output(3,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(3,GPIO.LOW)
     time.sleep(1)
