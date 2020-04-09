5681dc303050ff05c10016310e22a15a3fa3e91a
Charlie Fisher <fosh@uw.edu>
2020-04-08 18:44:56 -0700

    sending signal

--- integration/runwholecode.py
+++ integration/runwholecode.py
@@ -1,11 +1,14 @@
 #
 from datetime import datetime
+import RPi.GPIO as GPIO
+import time
 
 # datetime object containing current date and time
 now = datetime.now()
  
-print("now =", now)
+str_time = now.strftime("%H:%M:%S")
+print("time =", str_time)
 
-# dd/mm/YY H:M:S
-dt_string = now.strftime("%H:%M:%S")
-print("date and time =", dt_string)
+for i in range(5):
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(1)
