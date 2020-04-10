aca0f2be6394e725007977a814f526d232870ed2
Charlie Fisher <fosh@uw.edu>
2020-04-10 11:44:14 -0700

    test

--- integration/BlinkingLED.py
+++ integration/BlinkingLED.py
@@ -2,6 +2,7 @@ import time
 import serial
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
+from serial import Serial
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
