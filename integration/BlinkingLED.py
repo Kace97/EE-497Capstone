0b10cea8d14d709b97c6e3e3c003ab191b8d6437
Charlie Fisher <fosh@uw.edu>
2020-04-10 12:10:15 -0700

    test

--- integration/BlinkingLED.py
+++ integration/BlinkingLED.py
@@ -7,7 +7,8 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(3, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
 """
-ser = serial.Serial('/dev/ttyACM0',9600)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
 """
 def readByte(sentence, ser): # tests if Raspi can read from Arduino
         line = ""
@@ -19,14 +20,14 @@ def readByte(sentence, ser): # tests if Raspi can read from Arduino
         GPIO.output(3, GPIO.LOW) # Turn off LED
 """
 def sendByte(sentence, ser):
-        ser.write(sentence)
+        ser.write(sentence.encode('ascii'))
         #line = ser.readline().decode('utf-8').rstrip()
         print("trying to work")
         
 
 while True: 
     #readByte("on", ser)
-    sendByte('5', ser)
+    sendByte("5\n", ser)
     time.sleep(5)
     
     
