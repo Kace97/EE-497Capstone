ad6060c777de143b6c354f1b472eeda3d8834f30
Charlie Fisher <fosh@uw.edu>
2020-04-23 14:56:06 -0700

    rearrange files

--- Charlie_webapp/app.py
+++ Charlie_webapp/app.py
@@ -1,10 +1,42 @@
 from flask import Flask, render_template, redirect, url_for, send_file, render_template_string
 import datetime
 import requests
+import remi.gui as gui
+from remi import start, App
+import webbrowser
+"""
+class MyApp(App):
+    def __init__(self, *args):
+        super(MyApp, self).__init__(*args)
 
-app = Flask(__name__,template_folder='Templates/')
+    def main(self):
+        container = gui.VBox(width=120, height=100)
+        self.lbl = gui.Label('Hello world!')
+        self.bt = gui.Button('Press me!')
 
-response = requests.get('https://api.github.com')
+        # setting the listener for the onclick event of the button
+        self.bt.onclick.do(self.on_button_pressed)
+
+        # appending a widget to another, the first argument is a string key
+        container.append(self.lbl)
+        container.append(self.bt)
+
+        # returning the root widget
+        return container
+
+    # listener function
+    def on_button_pressed(self, widget):
+        self.lbl.set_text('Button pressed!')
+        self.bt.set_text('Hi!')
+
+    
+
+# starts the web server
+start(MyApp)
+
+"""
+
+app = Flask(__name__)
 
 # Set up title, headers, etc for home page
 def template(title = "PILL WEB APP", text = "Home Page"):
@@ -19,10 +51,43 @@ def template(title = "PILL WEB APP", text = "Home Page"):
 
 # Set home page
 @app.route("/")
-def hello():
+def home():
     templateData = template()
-    return render_template('main.html', **templateData)
+    return render_template('index.html', **templateData)
 
+@app.route("/button2")
+def button2():
+    message = "You went to page two"
+
+    templateData = template(title='Sign up', text = message)
+    return render_template('signup.html', **templateData)
+
+if __name__ == '__main__':
+    webbrowser.open_new("http://127.0.0.1:5000/")
+    app.run(debug=True, host='0.0.0.0')
+
+"""
+# Set up title, headers, etc for home page
+def template(title = "PILL WEB APP", text = "Home Page"):
+    now = datetime.datetime.now()
+    timeString = now
+    templateData = {
+        'title' : title,
+        'time' : timeString,
+        'text' : text
+        }
+    return templateData
+@app.route("/data")
+def index():
+	# Read Sensors Status
+	buttonSts = GPIO.input(button)
+	senPIRSts = GPIO.input(senPIR)
+	templateData = {
+      'title' : 'GPIO input Status!',
+      'button'  : buttonSts,
+      'senPIR'  : senPIRSts
+      }
+	return render_template('index.html', **templateData)
 @app.route("/button1")
 def button1():
     message = "You went to page one"
@@ -41,23 +106,4 @@ def button2():
 def home():
     templateData = template()
     return render_template('main.html', **templateData)
-
-@app.route("/data")
-def index():
-	# Read Sensors Status
-	buttonSts = GPIO.input(button)
-	senPIRSts = GPIO.input(senPIR)
-	templateData = {
-      'title' : 'GPIO input Status!',
-      'button'  : buttonSts,
-      'senPIR'  : senPIRSts
-      }
-	return render_template('index.html', **templateData)
-
-if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
-
-
-
-
-
+"""
